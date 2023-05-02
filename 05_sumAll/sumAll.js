57e24fd58b72b432a69cd32547aab76a374f3c86
Keago99 <keatabde@gmail.com>
2023-05-02 15:57:35 +0200

    complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let totNum = 0;
+    
+    if (!Number.isInteger(start) || !Number.isInteger(end)){
+        return 'ERROR';
+    }
 
+    if (start > end){
+        const temp = start;
+        start = end;
+        end = temp;
+    }
+
+    if (start < 0 || end < 0){
+        return "ERROR";
+    }
+
+    for(i = start; i < end+1; i++){
+
+        totNum = totNum + i; 
+    }
+    return totNum;
 };
 
 // Do not edit below this line
