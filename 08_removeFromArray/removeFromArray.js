c3e01a2230e86c0989e2a91b9e7ce5d801171369
Keago99 <keatabde@gmail.com>
2025-07-26 23:34:31 +0200

    add logic, however the solution is a far easier solution, a reminder to really think about the simplest solution. Complete!

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...args) {
+   for (let i = 0; i < args.length; i++) {
+        const arg = args[i];
+        while (array.indexOf(arg) > -1) {
+            const index = array.indexOf(arg);
+            array.splice(index, 1);
+        }
+    }
+    return array;
+}
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
