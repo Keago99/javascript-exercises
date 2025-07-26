6db5e2773f3e7470c51ffedb2d7df027045b5fc8
Keago99 <keatabde@gmail.com>
2025-07-27 00:02:51 +0200

    logic for sumAll added, this was a tough one, not the most elegant code, but it works!

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(first, second) {
+    let sum = 0;
+    if (Number.isInteger(first) && first > 0 && Number.isInteger(second) && second > 0){
+        if (first < second){
+            for (let i = first; i <= second; i++){
+            sum += i;
+            }
+            return sum;
+        }
+        else{
+            for (let i = second; i <= first; i++){
+                sum += i;
+            }
+            return sum;
+        }
+    }
+    else{
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
