cd2d416fdcefe604cf01417b1f87d8121c6dd83b
Keago99 <keatabde@gmail.com>
2025-07-27 16:50:28 +0200

    Added all working logic for tempConversion, Complete!

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let converted = (temp - 32) * 5 / 9;
+  let roundedConverted = Math.round(converted * 10) / 10;
+  return roundedConverted;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp1) {
+  let converted1 = (temp1 * 9/5) + 32;
+  let roundedConverted1 = Math.round(converted1 * 10) / 10;
+  return roundedConverted1;
 };
 
 // Do not edit below this line
