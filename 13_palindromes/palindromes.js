20302d5ffb7f4b6ef1a846c010fce105e6896e5b
Keago99 <keatabde@gmail.com>
2025-11-06 21:07:52 +0200

    Complete palindromes
    All tests passing

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
 
+
+
+
+const palindromes = function (word) {
+    const wordArray = word.toLowerCase().split("");
+    const alphabet = "abcdefghijklmnopqrstuvwxyz1234567890";
+    const filteredArray = wordArray.filter((char) => {
+        if(alphabet.includes(char)){
+            return char;
+        }
+    });
+    const reversedArray = filteredArray.toReversed();
+    if (JSON.stringify(filteredArray) === JSON.stringify(reversedArray)){
+        return true;
+    }
+    else{
+        return false;
+    }
 };
 
 // Do not edit below this line
