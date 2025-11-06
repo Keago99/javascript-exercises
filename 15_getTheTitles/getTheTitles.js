c963cff8ac67e5bfa2a5b5c082abddc7fb6871ee
Keago99 <keatabde@gmail.com>
2025-11-06 21:30:54 +0200

    working solution for titles.

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+  return array.reduce((titles, book) => {
+    return [...titles, book.title];
+  }, []);
 };
 
 // Do not edit below this line
