542dde5bd6b4368d144a561fac464982aee02ffb
Keago99 <keatabde@gmail.com>
2025-11-06 18:41:09 +0200

    Complete 12_calculator tests
    All tests passing.

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+  const arrayTotal = numArray.reduce((accumulator, current) => accumulator + current, 0);
+  return arrayTotal;
 };
 
-const multiply = function() {
-
+const multiply = function(numArray) {
+  const multiArray = numArray.reduce((accumulator, current) => accumulator * current);
+  return multiArray;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+    if (n < 0) return undefined;
+    if (n === 0 || n === 1) return 1;
+    
+    let result = 1;
+    for (let i = 2; i <= n; i++) {
+        result *= i;
+    }
+    return result;
 };
 
 // Do not edit below this line
