3817e54a2cfb5b0508f5d29ba40fe3309ac89261
Keago99 <keatabde@gmail.com>
2025-11-07 12:58:13 +0200

    Add logic to complete tests
    All tests passing, my own logic!

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
-};
+    //create an array with the ages a new derived property
+    const ageArray = people.map(person =>{
+        const deathYear = person.yearOfDeath || 2025;
+        return deathYear - person.yearOfBirth;
+    });
+    const highestNumber = Math.max(...ageArray);
+    const indexOfOldest = ageArray.indexOf(highestNumber)
+    return people[indexOfOldest];
+
+   
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
